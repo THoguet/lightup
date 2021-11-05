bbdd857adcf0cc68b6c88a74992bd637d3218ab5
Thibault Perrier <thibault.perrier.1@etu.u-bordeaux.fr>
2021-11-05 18:25:12 +0100

    base struct

--- game_test_thperrier.c
+++ game_test_thperrier.c
@@ -1,7 +1,11 @@
-void test_game_text(){
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
+void test_game_text(){
+    return EXIT_SUCCESS;
 }
 
 int main (int argc, char *argv[]){
-    
+    return EXIT_SUCCESS;
 }
