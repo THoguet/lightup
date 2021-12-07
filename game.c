3946e3e5f1e5052ca7d7d5ca8594dc16a9988290
Thibault Perrier <thibault.perrier.1@etu.u-bordeaux.fr>
2021-12-07 01:35:05 +0100

    edit game_delete

--- game.c
+++ game.c
@@ -64,8 +64,8 @@ bool game_equal(cgame g1, cgame g2) {
 void game_delete(game g) {
 	for (int i = 0; i < DEFAULT_SIZE; i++) {
 		free(g->tab_cell[i]);
-		g->tab_cell[i] = NULL;
 	}
+	free(g->tab_cell);
 	free(g);
 	g = NULL;
 	return;
