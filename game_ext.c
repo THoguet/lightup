0c17df162ca549223d000d143fa5b0a74b84c2d8
Théophyl Hoguet <theophyl.hoguet@etu.u-bordeaux.fr>
2021-12-09 10:14:46 +0100

    empty proto

--- game_ext.c
+++ game_ext.c
@@ -1,3 +1,30 @@
-#include <stdio.h>
-#include <stdio.h>
 #include "game_ext.h"
+#include <stdio.h>
+
+game game_new_ext(uint nb_rows, uint nb_cols, square* squares, bool wrapping) {
+	return;
+}
+
+game game_new_empty_ext(uint nb_rows, uint nb_cols, bool wrapping) {
+	return;
+}
+
+uint game_nb_rows(cgame g) {
+	return;
+}
+
+uint game_nb_cols(cgame g) {
+	return;
+}
+
+bool game_is_wrapping(cgame g) {
+	return;
+}
+
+void game_undo(game g) {
+	return;
+}
+
+void game_redo(game g) {
+	return;
+}
