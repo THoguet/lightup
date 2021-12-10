3649e8207584b343c793a060f0b1612608e530f9
Théophyl Hoguet <theophyl.hoguet@etu.u-bordeaux.fr>
2021-12-10 15:44:47 +0100

    game_new_ext and game_new_empty_ext

--- game_ext.c
+++ game_ext.c
@@ -3,11 +3,44 @@
 #include <stdlib.h>
 
 game game_new_ext(uint nb_rows, uint nb_cols, square* squares, bool wrapping) {
-	return;
+	game g = game_new_empty_ext(nb_rows, nb_cols, wrapping);
+	for (int i = 0; i < g->height; i++) {
+		for (int j = 0; j < g->width; j++) {
+			g->tab_cell[i][j] = squares[g->width * i + j];
+		}
+	}
+	return g;
 }
 
 game game_new_empty_ext(uint nb_rows, uint nb_cols, bool wrapping) {
+	// alloc struct
 	game g = (game)malloc(sizeof(struct game_s));
+	if (g == NULL) {
+		fprintf(stderr, "not enough memory\n");
+		exit(EXIT_FAILURE);
+	}
+	// set variable of struct
+	g->wrapped = wrapping;
+	g->height = nb_rows;
+	g->width = nb_cols;
+	g->hist = history_create_empty();
+	// alloc the tab with variables set before
+	g->tab_cell = (square**)malloc(sizeof(square*) * g->height);
+	if (g->tab_cell == NULL) {
+		fprintf(stderr, "not enough memory\n");
+		exit(EXIT_FAILURE);
+	}
+	for (int i = 0; i < g->height; i++) {
+		g->tab_cell[i] = (square*)malloc(sizeof(square) * g->width);
+		if (g->tab_cell[i] == NULL) {
+			fprintf(stderr, "not enough memory\n");
+			exit(EXIT_FAILURE);
+		}
+		for (int j = 0; j < g->width; j++) {
+			g->tab_cell[i][j] = S_BLANK;
+		}
+	}
+	return g;
 }
 
 uint game_nb_rows(cgame g) {
