fafb1550fe054980a9fa9f66b29d7ed08ff62a43
Théophyl Hoguet <theophyl.hoguet@etu.u-bordeaux.fr>
2022-01-15 22:19:36 +0100

    add comments + errpoitner

--- game_private.c
+++ game_private.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int max(int a, int b) {
 	if (a > b)
 		return a;
 	return b;
+}
+
+void checkPointer(void* p) {
+	if (!p) {
+		fprintf(stderr, "Error on given pointer.\n");
+		exit(EXIT_FAILURE);
+	}
 }
--- game_private.h
+++ game_private.h
@@ -3,13 +3,29 @@
 #include "history.h"
 
 struct game_s {
+	// tab of tab of square (eg: tab_cell[i][j])
 	square** tab_cell;
 	bool wrapping;
 	uint height;  // i in general
 	uint width;   // j in general
+	// case of history
 	history hist;
 };
 
+/**
+ * @brief return the max between a and b
+ *
+ * @param a int to test
+ * @param b int to test
+ * @return the max between a and b
+ */
 int max(int a, int b);
+
+/**
+ * @brief check if the pointer is NULL fprinf and exit else nothing
+ *
+ * @param p any pointer
+ */
+void checkPointer(void* p);
 
 #endif
