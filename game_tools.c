422b57047c5c93321ca6d1270a91e7b90ee1951f
Mohamed Fatih <mfatih001@emi.u-bordeaux.fr>
2022-02-09 17:57:27 +0100

    add fichier game_tools

---
+++ game_tools.c
@@ -0,0 +1,14 @@
+#include "game_tools.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "game.h"
+
+game game_load(char* filename) {
+	return NULL;
+}
+
+void game_save(cgame g, char* filename) {
+	return;
+}
