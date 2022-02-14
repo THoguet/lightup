20b626d1aed37f0752e4766738ff1663bd89550a
Mohamed Fatih <mohamed.fatih@etu.u-bordeaux.fr>
2022-02-14 12:51:03 +0100

    end fonction game_load (closes #4)

--- game_tools.c
+++ game_tools.c
@@ -17,39 +17,44 @@ game game_load(char* filename) {
 		exit(EXIT_FAILURE);
 	}
 	// récuperer les valeurs dont on a beson pour créer le jeu initail
-	uint nb_rows = fscanf(f, "%u", &nb_rows), nb_cols = fscanf(f, "%u", &nb_cols);
-	int wrapping = fscanf(f, "%d", &wrapping);
+	uint nb_rows, nb_cols;
+	int wrapping;
+	if (fscanf(f, "%u %u %d\n", &nb_rows, &nb_cols, &wrapping) != 3) {
+		fprintf(stderr, "Error in scan of file");
+		exit(EXIT_FAILURE);
+	}
 	// créer le jeu initail
 	game g1 = game_new_empty_ext(nb_rows, nb_cols, wrapping);
 	checkPointer((void*)g1);
 	// récuperer les coups à jouer
 	for (uint i = 0; i < nb_rows; i++) {
 		for (uint j = 0; j < nb_cols; j++) {
-			while (1) {
-				// obtenir les squares pour les jouer dans g1
-				char play = fgetc(f);
-				if (feof(f)) {
-					break;
-				}
-				if (play == 'b')
-					game_set_square(g1, i, j, S_BLANK);
-				else if (play == '*')
-					game_set_square(g1, i, j, S_LIGHTBULB);
-				else if (play == '-')
-					game_set_square(g1, i, j, S_MARK);
-				else if (play == 'w')
-					game_set_square(g1, i, j, S_BLACKU);
-				else if (play == '0')
-					game_set_square(g1, i, j, S_BLACK0);
-				else if (play == '1')
-					game_set_square(g1, i, j, S_BLACK1);
-				else if (play == '2')
-					game_set_square(g1, i, j, S_BLACK2);
-				else if (play == '3')
-					game_set_square(g1, i, j, S_BLACK3);
-				else if (play == '4')
-					game_set_square(g1, i, j, S_BLACK4);
+			if (feof(f)) {
+				fprintf(stderr, "end of file");
+				exit(EXIT_FAILURE);
 			}
+			// obtenir les squares pour les jouer dans g1
+			char play = fgetc(f);
+			if (play == '\n')
+				play = fgetc(f);
+			if (play == 'b')
+				game_set_square(g1, i, j, S_BLANK);
+			else if (play == '*')
+				game_set_square(g1, i, j, S_LIGHTBULB);
+			else if (play == '-')
+				game_set_square(g1, i, j, S_MARK);
+			else if (play == 'w')
+				game_set_square(g1, i, j, S_BLACKU);
+			else if (play == '0')
+				game_set_square(g1, i, j, S_BLACK0);
+			else if (play == '1')
+				game_set_square(g1, i, j, S_BLACK1);
+			else if (play == '2')
+				game_set_square(g1, i, j, S_BLACK2);
+			else if (play == '3')
+				game_set_square(g1, i, j, S_BLACK3);
+			else if (play == '4')
+				game_set_square(g1, i, j, S_BLACK4);
 		}
 	}
 	fclose(f);
