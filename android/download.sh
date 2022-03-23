29ddb79444da8ae05c4ff3b3e0258e35ec3e2fa4
Théophyl Hoguet <theophyl.hoguet@etu.u-bordeaux.fr>
2022-03-23 21:49:38 +0100

    update download.sh

--- android/download.sh
+++ android/download.sh
@@ -2,16 +2,20 @@
 
 cd jni
 
-wget -c https://www.libsdl.org/release/SDL2-2.0.5.zip
-unzip SDL2-2.0.5.zip
-mv SDL2-2.0.5 SDL
+wget -c https://www.libsdl.org/release/SDL2-2.0.20.zip
+unzip SDL2-2.0.20.zip
+mv SDL2-2.0.20 SDL
 
-wget -c https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.zip
-unzip SDL2_ttf-2.0.14.zip
-mv SDL2_ttf-2.0.14 SDL_ttf
+wget -c https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.18.zip
+unzip SDL2_ttf-2.0.18.zip
+mv SDL2_ttf-2.0.18 SDL_ttf
 
-wget -c https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.zip
-unzip SDL2_image-2.0.1.zip
-mv SDL2_image-2.0.1 SDL_image
+wget -c https://libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.zip
+unzip SDL2_mixer-2.0.4.zip
+mv SDL2_mixer-2.0.4 SDL_mixer
+
+wget -c https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.zip
+unzip SDL2_image-2.0.5.zip
+mv SDL2_image-2.0.5 SDL_image
 
 cd ..
